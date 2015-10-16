9f77d95721796e4f13545e594a3f08e9e7db3822
suyu34 <huaqiao@teambition.com>
2015-10-16 23:40:20 +0800

    Run stardard

--- gulp/tasks/format.js
+++ gulp/tasks/format.js
@@ -6,8 +6,7 @@ var rename = require('gulp-rename')
 var sequence = require('gulp-sequence')
 
 module.exports = function (gulp) {
-
-  gulp.task('format-design',function () {
+  gulp.task('format-design', function () {
     return gulp.src(['design/**/*.js', '!design/**/*.min.js'])
       .pipe(esformatter())
       .pipe(gulp.dest('design'))
